1e5ca26ba3da4cf5386a54cd9e55ee1697adfeb9
Alex Campbell <campbellar2@vcu.edu>
2022-04-15 09:25:41 -0400

    added operator function

--- script.js
+++ script.js
@@ -11,3 +11,11 @@ function multiply(num1, num2) {
 function divide(num1, num2) {
     return num1 / num2;
 }
+
+//operator function
+function operate(operator, num1, num2) {
+    if (operator === "+") { return add(num1, num2) }
+    else if (operator === "-") { return subtract(num1, num2) }
+    else if (operator === "*") { return multiply(num1, num2) }
+    else { return divide(num1, num2) };
+}
