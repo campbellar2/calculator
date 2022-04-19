7a836f6211160c5bd600af87f4f4bf9269f8f285
Alex Campbell <campbellar2@vcu.edu>
2022-04-18 21:46:51 -0400

    display function added

--- script.js
+++ script.js
@@ -1,12 +1,22 @@
 //document selectors
 const buttons = document.querySelectorAll("button");
-
+//important global variables
+let displayValue = '0';
 
 //connect buttons to keyboard
 window.addEventListener('keydown', function(e) {
     const key = document.querySelector(`button[data-key=${e.key}]`);
     key.click();
 });
+
+//update display function
+function updateDisplay() {
+    const display = document.getElementById("display");
+    display.innerText = displayValue;
+    if (displayValue.length > 9) {
+        display.innerText = displayValue.substring(0, 9);
+    };
+}
 
 //basic math operations functions
 function add(num1, num2) {
